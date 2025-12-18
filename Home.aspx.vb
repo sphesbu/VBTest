f1e602207f03cd7cb28955cfde93c880c2ff73a6
SpheDev <81554170+sphesbu@users.noreply.github.com>
2025-12-18 22:19:55 +0200

    Update Home.aspx.vb

--- Home.aspx.vb
+++ Home.aspx.vb
@@ -1,23 +1,30 @@
-﻿Public Class Home
-    Inherits System.Web.UI.Page
+Public Class GoogleMap
+    Implements System.Web.IHttpHandler
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not IsPostBack Then
-            Dim roles As New List(Of ListItem) From {
-                New ListItem("Admin", "1"),
-                New ListItem("Editor", "2"),
-                New ListItem("Viewer", "3")
-            }
+    Sub ProcessRequest(ByVal context As HttpContext) Implements IHttpHandler.ProcessRequest
+        context.Response.ContentType = "application/json"
 
-            ddlStatus.DataSource = roles
-            ddlStatus.DataTextField = "Text"
-            ddlStatus.DataValueField = "Value"
-            ddlStatus.DataBind()
+        Dim address As String = context.Request.QueryString("address")
 
-            ' Preselect Editor (ID = 2)
-            ddlStatus.SelectedValue = "2"
-        End If
 
+        Dim lat As Double = 0
+        Dim lng As Double = 0
+
+        getMap(address, lat, lng)
+
+        Dim json As String = "{""lat"": " & lat & ", ""lng"": " & lng & "}"
+        context.Response.Write(json)
     End Sub
+    Private Sub getMap(address As String, ByRef lat As Double, ByRef lng As Double)
+
+        lat = -29.8587
+        lng = 31.0218
+    End Sub
+
+    ReadOnly Property IsReusable() As Boolean Implements IHttpHandler.IsReusable
+        Get
+            Return False
+        End Get
+    End Property
 
 End Class
